94df404ba6563f004e43e79cd7bd2f907870307d
Silvia Gomez <silvia.gomez@unidadeditorial.es>
2024-03-13 18:11:57 +0100

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-// https://astro.build/config
+
 export default defineConfig({
     
     integrations: [svelte()],
-
     build: {
-        inlineStylesheets: `never`,
-        format:'file',
-      },
+        inlineStylesheets: 'never',
+        format: 'directory'
+    }
 });
